33b46e34ddc037b75644ac1a2a19cabf683c2562
YSW2 <jrgyber@gmail.com>
2023-07-30 23:24:12 +0900

    comment commit

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect
+from posts.models import Post, Comment
+from posts.forms import CommentForm
 
 # Create your views here.
 
@@ -6,4 +10,30 @@ def feeds(request):
     if not request.user.is_authenticated:
         return redirect("/users/login/")
 
-    return render(request, "posts/feeds.html")
+    posts = Post.objects.all()
+    comment_form = CommentForm()
+    context = {
+        "posts": posts,
+        "comment_form": comment_form,
+    }
+    return render(request, "posts/feeds.html", context)
+
+@require_POST
+def comment_add(request):
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.save()
+
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+
+@require_POST
+def comment_delete(request, comment_id):
+    if request.method == 'POST':
+        comment = Comment.objects.get(id=comment_id)
+        if comment.user == request.user:
+            comment.delete()
+            return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+    else:
+        return HttpResponseForbidden("삭제 권한이 없습니다")
