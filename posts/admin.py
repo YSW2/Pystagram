1292f38be57782cd0d2baadc6e6ae1e99fa1310c
YSW2 <jrgyber@gmail.com>
2023-08-04 01:33:06 +0900

    like commit

--- posts/admin.py
+++ posts/admin.py
@@ -16,6 +16,15 @@ class PostImageInline(admin.TabularInline):
     model = PostImage
     extra = 1
 
+class LikeUserInline(admin.TabularInline):
+    model = Post.like_users.through
+    verbose_name = "좋아요 한 User"
+    verbose_name_plurar = f"{verbose_name} 목록"
+    extra = 1
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
@@ -25,6 +34,7 @@ class PostAdmin(admin.ModelAdmin):
     inlines = [
         CommentInline,
         PostImageInline,
+        LikeUserInline,
     ]
 
     formfield_overrides = {
